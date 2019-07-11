f48a76fbd4b217a7518d2ddc5298a45c06f9a93d
pawel <pjastrz@gmail.com>
2019-07-11 11:02:14 +0300

    generate docker-compose for specified amount of nodes

--- src/main/java/com/hazelcast/hazelcompose/DockerComposeGenerator.java
+++ src/main/java/com/hazelcast/hazelcompose/DockerComposeGenerator.java
@@ -5,11 +5,17 @@ import org.jtwig.JtwigTemplate;
 
 class DockerComposeGenerator {
 
-    String generateDockerCompose(int nodes) {
+    String generateDockerCompose(int membersCount,
+                                 int initialMemberPort,
+                                 boolean includeManCenter,
+                                 int manCenterExposedPort) {
         JtwigTemplate template = JtwigTemplate.classpathTemplate("templates/docker-compose.twig");
         JtwigModel model = JtwigModel
                 .newModel()
-                .with("nodes", nodes);
+                .with("membersCount", membersCount)
+                .with("initialMemberPort", initialMemberPort)
+                .with("includeManCenter", includeManCenter)
+                .with("manCenterExposedPort", manCenterExposedPort);
         return template.render(model);
     }
 }
--- src/main/java/com/hazelcast/hazelcompose/Main.java
+++ src/main/java/com/hazelcast/hazelcompose/Main.java
@@ -1,10 +1,12 @@
 package com.hazelcast.hazelcompose;
 
-import java.io.FileNotFoundException;
-
 public class Main {
     public static void main(String[] args) {
-        String dockerCompose = new DockerComposeGenerator().generateDockerCompose(3);
+        String dockerCompose = new DockerComposeGenerator().generateDockerCompose(3,
+                5702,
+                false,
+                8081);
+
         System.out.println(dockerCompose);
     }
 }
