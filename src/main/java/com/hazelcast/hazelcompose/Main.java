2e894d91cf159bd6ef5ed744988fcfc4e48228c7
Andrey Pechkurov <apechkurov@gmail.com>
2019-07-11 11:37:02 +0300

    Integrate with DockerComposeGenerator

--- src/main/java/com/hazelcast/hazelcompose/Main.java
+++ src/main/java/com/hazelcast/hazelcompose/Main.java
@@ -6,25 +6,43 @@ import java.io.PrintWriter;
 import java.util.HashMap;
 import java.util.Map;
 
+import static com.hazelcast.hazelcompose.ArgUtil.parseBoolean;
+import static com.hazelcast.hazelcompose.ArgUtil.parseInt;
+import static com.hazelcast.hazelcompose.Main.Argument.IMDG_CLUSTER_SIZE;
+import static com.hazelcast.hazelcompose.Main.Argument.IMDG_START_PORT;
+import static com.hazelcast.hazelcompose.Main.Argument.MC_ENABLE;
+import static com.hazelcast.hazelcompose.Main.Argument.MC_PORT;
+
 public class Main {
 
-    private enum Argument {
+    public enum Argument {
 
-        MC_PORT("MC port (default: 8080)", "8080"),
+        MC_ENABLE("Include Management Center (default: true)", "true"),
+        MC_PORT("Management Center port (default: 8080)", "8080"),
         IMDG_START_PORT("IMDG port, incremented for each additional member (default: 5701)", "5701"),
         IMDG_CLUSTER_SIZE("IMDG cluster size (default: 3)", "3");
 
         final String descr;
-        final String defValue;
+        final String defVal;
 
-        Argument(String descr, String defValue) {
+        Argument(String descr, String defVal) {
             this.descr = descr;
-            this.defValue = defValue;
+            this.defVal = defVal;
         }
 
     }
 
-    public static void main(String[] args) {
+    private static String generate(Map<Argument, String> args) {
+        return new DockerComposeGenerator()
+                .generateDockerCompose(
+                        parseInt(args.get(IMDG_CLUSTER_SIZE), IMDG_CLUSTER_SIZE.defVal),
+                        parseInt(args.get(IMDG_START_PORT), IMDG_START_PORT.defVal),
+                        parseBoolean(args.get(MC_ENABLE), MC_ENABLE.defVal),
+                        parseInt(args.get(MC_PORT), MC_PORT.defVal)
+                );
+    }
+
+    public static void main(String[] arguments) {
         try {
             ConsoleReader in = new ConsoleReader();
             in.setPrompt("> ");
@@ -32,7 +50,7 @@ public class Main {
             PrintWriter out = new PrintWriter(in.getOutput());
             out.println("Welcome to HazelCompose. Please enter parameter values to generate IMDG Docker Compose file.");
 
-            Map<String, String> argValues = new HashMap<>();
+            Map<Argument, String> args = new HashMap<>();
             for (Argument arg : Argument.values()) {
                 out.println("Enter " + arg.descr + ":");
                 String line = in.readLine();
@@ -40,15 +58,11 @@ public class Main {
                     out.println("Stopping generator...");
                     return;
                 }
-
-                if (!line.trim().isEmpty()) {
-                    argValues.put(arg.name(), line);
-                } else {
-                    argValues.put(arg.name(), arg.defValue);
-                }
+                args.put(arg, line);
             }
 
-            System.out.println(argValues);
+            out.println();
+            System.out.println(generate(args));
         } catch (Throwable t) {
             t.printStackTrace();
         }
